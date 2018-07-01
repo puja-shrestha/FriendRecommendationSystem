45b898eb86b035851cc493f6ac2d19a33731255c
Luckey <luckeysherpa07@gmail.com>
2018-07-02 00:29:40 +0545

    Login Added

--- app/src/main/java/com/example/puza/friendrecommendation/LoginActivity.java
+++ app/src/main/java/com/example/puza/friendrecommendation/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.puza.friendrecommendation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,8 +13,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FirebaseAuth mAuth;
     private Button buttonLogin;
     private EditText password;
     private EditText email;
@@ -25,17 +32,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+
         password = (EditText)findViewById(R.id.password);
         email = (EditText)findViewById(R.id.email);
 
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        } );
 
         textViewRegister = (TextView) findViewById(R.id.textViewRegister);
         textViewRegister.setOnClickListener(new View.OnClickListener(){
@@ -45,5 +49,33 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         } );
+    }
+
+    private void userLogin() {
+        String emailText = email.getText().toString().trim();
+        String passwordText = password.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(emailText, passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLogin:
+                userLogin();
+                break;
+        }
     }
 }
