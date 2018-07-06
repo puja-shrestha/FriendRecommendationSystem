b468614c0e8f98274f6d1b2740f3244e05a80e38
Luckey <luckeysherpa07@gmail.com>
2018-07-06 21:17:24 +0545

    Added Sign Up Database

--- app/src/main/java/com/example/puza/friendrecommendation/model/User.java
+++ app/src/main/java/com/example/puza/friendrecommendation/model/User.java
@@ -6,10 +6,49 @@ public class User {
     private String name;
     private String description;
 
+    private String FirstName;
+    private String LastName;
+    private String Address;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String address) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Address = address;
+    }
+
+
     public User(int image, String name, String description) {
         this.image = image;
         this.name = name;
         this.description = description;
+    }
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
     }
 
     public int getImage() {
@@ -35,4 +74,7 @@ public class User {
     public void setDescription(String description) {
         this.description = description;
     }
+
+
+
 }
